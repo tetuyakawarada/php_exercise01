fd4224b04f3b2be3888006c9680fc8d4d2eae7a5
tetuyakawarada <tetuya.kawarada@gmail.com>
2022-09-06 23:54:12 +0900

    関数の課題

---
+++ 08/01_functions.php
@@ -0,0 +1,14 @@
+<?php
+
+// '朝' or '昼' or '夜'を指定
+$time_zone = 'XXX';
+
+function get_greeting($time_zone)
+{
+    $greetings = [
+        '朝' => 'おはよう',
+        '昼' => 'こんにちは',
+        '夜' => 'こんばんは'
+    ];
+    return $time_zone . 'の挨拶は' . $greetings[$time_zone];
+}
